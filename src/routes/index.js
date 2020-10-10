2c8e9d8fa78ed398bbe93818322c414d2581b59d
Matheus Lira Sartor <matheus.sartor@radixeng.com.br>
2020-10-10 13:17:50 -0300

    Commit_Teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,14 @@
-const express = require('express');
-const router = express.Router();
+import cors from 'cors';
+import express, { request, response } from 'express';
+import router from './personRoute'
+import path from 'path'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.listen(3333);
 
 router.get('/', function (req, res, next) {
   res.status(200).send({
